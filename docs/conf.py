bbf94cb253c09e4dc8d16fd281a6cf8791ffdf21
Moritz <moritz.willmann@ipa.fraunhofer.de>
2023-08-30 14:09:11 +0200

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = "2023, Fraunhofer IPA"
 author = "Fraunhofer IPA"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 
