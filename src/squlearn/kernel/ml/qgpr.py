d54bf928062087f62a31ecd476d7a106679b4c5c
mcr <marco.r.roth@gmail.com>
2023-09-20 11:32:53 +0200

    add additional information about kwargs in quantum kernel methods

--- src/squlearn/kernel/ml/qgpr.py
+++ src/squlearn/kernel/ml/qgpr.py
@@ -17,6 +17,7 @@ class QGPR(BaseEstimator, RegressorMixin):
     This class implements the Gaussian process regression analogous to scikit-learn
     but is not a wrapper.
     The implementation is based on Algorithm 2.1 of Ref. [1].
+    Additional arguments can be set via ``**kwargs``.
 
     Args:
         quantum_kernel (KernelMatrixBase):
@@ -34,6 +35,10 @@ class QGPR(BaseEstimator, RegressorMixin):
                 unit-variance priors are used. Note that, in this implementation,
                 the normalization is reversed before the GP predictions are reported.
         full_regularization: (bool), default=True: enable full gram matrix regularization.
+        **kwargs: Keyword arguments for the quantum kernel matrix, possible arguments can be obtained
+            by calling ``get_params()``. Can be used to set for example the number of qubits
+            (``num_qubits=``), or (if supported) the number of layers (``num_layers=``)
+            of the underlying feature map.
 
     See Also
     --------
