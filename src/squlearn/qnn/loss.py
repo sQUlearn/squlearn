fix gradient, add tests

src/squlearn/qnn/loss.py
@@ -440,7 +440,7 @@ class LogLoss(LossBase):
             probability_values = np.stack([probability_values, 1.0 - probability_values], axis=1)
             ground_truth = np.stack([ground_truth, 1.0 - ground_truth], axis=1)
 
-        loss = -1.0 * np.sum(
+        loss = -1.0 * np.mean(
             np.multiply(
                 np.sum(ground_truth * np.log(probability_values), axis=1),
                 weights,
@@ -474,32 +474,36 @@ class LogLoss(LossBase):
             raise AttributeError("SquaredLoss requires ground_truth.")
 
         ground_truth = kwargs["ground_truth"]
-        weights = kwargs.get("weights", np.ones_like(ground_truth))
+        weights = kwargs.get("weights", np.ones(ground_truth.shape[0]))
         multiple_output = kwargs.get("multiple_output", False)
 
         probability_values = np.clip(value_dict["f"], self._eps, 1.0 - self._eps)
-        if probability_values.ndim == 1:
+        binary = probability_values.ndim == 1
+        if binary:
             probability_values = np.stack([probability_values, probability_values - 1.0], axis=1)
             ground_truth = np.stack([ground_truth, 1.0 -  ground_truth], axis=1)
 
         weighted_outer_gradient = np.multiply(
-            np.sum(ground_truth / probability_values, axis=1),
-            weights,
+            ground_truth / probability_values,
+            np.tile(weights.reshape(-1,1), probability_values.shape[1]),
         )
 
+        if binary:
+            weighted_outer_gradient = np.sum(weighted_outer_gradient, axis=1)
+
         if multiple_output:
-            d_p = -1.0 * np.einsum("ij,ijk->k", weighted_outer_gradient, value_dict["dfdp"])
+            d_p = -1.0 * np.einsum("ij,ijk->k", weighted_outer_gradient, value_dict["dfdp"]) / ground_truth.shape[0]
         else:
-            d_p = -1.0 * np.einsum("j,jk->k", weighted_outer_gradient, value_dict["dfdp"])
+            d_p = -1.0 * np.einsum("j,jk->k", weighted_outer_gradient, value_dict["dfdp"]) / ground_truth.shape[0]
 
         # Extra code for the cost operator derivatives
         if not self._opt_param_op:
             return d_p
 
         if multiple_output:
-            d_op = -1.0 * np.einsum("ij,ijk->k", weighted_outer_gradient, value_dict["dfdop"])
+            d_op = -1.0 * np.einsum("ij,ijk->k", weighted_outer_gradient, value_dict["dfdop"]) / ground_truth.shape[0]
         else:
-            d_op = -1.0 * np.einsum("j,jk->k", weighted_outer_gradient, value_dict["dfdop"])
+            d_op = -1.0 * np.einsum("j,jk->k", weighted_outer_gradient, value_dict["dfdop"]) / ground_truth.shape[0]
         return d_p, d_op
 
 