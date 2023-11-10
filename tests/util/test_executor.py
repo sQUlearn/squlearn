cc28db76d353bd184d2d784086b8e8e0f32c247f
David Kreplin <david.kreplin@ipa.fraunhofer.de>
2023-11-10 13:39:37 +0100

    black

--- tests/util/test_executor.py
+++ tests/util/test_executor.py
@@ -1,85 +1,108 @@
 import pytest
 import numpy as np
 
-from qiskit import Aer,QuantumCircuit
+from qiskit import Aer, QuantumCircuit
 from qiskit.primitives import Estimator, Sampler, BackendEstimator, BackendSampler
 from qiskit.quantum_info import SparsePauliOp
 
 from squlearn.util import Executor
 
-class TestExecutor:
 
+class TestExecutor:
     @pytest.fixture(scope="module")
     def ExecutorSampler(self) -> Executor:
-        """Executor with Sampler initilization."""
-        return Executor(Sampler(),primitive_seed=0)
+        """Executor with Sampler initialization."""
+        return Executor(Sampler(), primitive_seed=0)
 
     @pytest.fixture(scope="module")
     def ExecutorEstimator(self) -> Executor:
-        """Executor with Estimator initilization."""
-        return Executor(Estimator(),primitive_seed=0)
+        """Executor with Estimator initialization."""
+        return Executor(Estimator(), primitive_seed=0)
 
     @pytest.fixture(scope="module")
     def ExecutorStatevector(self) -> Executor:
-        """Executor with statevector_simulator initilization."""
-        return Executor("statevector_simulator",primitive_seed=0)
+        """Executor with statevector_simulator initialization."""
+        return Executor("statevector_simulator", primitive_seed=0)
 
     @pytest.fixture(scope="module")
     def ExecutorQasm(self) -> Executor:
-        """Executor with qasm_simulator initilization."""
-        return Executor("qasm_simulator",primitive_seed=0)
+        """Executor with qasm_simulator initialization."""
+        return Executor("qasm_simulator", primitive_seed=0)
 
     @pytest.fixture(scope="module")
     def ExecutorBackendSampler(self) -> Executor:
-        """Executor with Sampler initilization."""
-        return Executor(BackendSampler(Aer.get_backend("qasm_simulator")),primitive_seed=0)
+        """Executor with BackendSampler initialization."""
+        return Executor(BackendSampler(Aer.get_backend("qasm_simulator")), primitive_seed=0)
 
     @pytest.fixture(scope="module")
     def ExecutorBackendEstimator(self) -> Executor:
-        """Executor with Estimator initilization."""
-        return Executor(BackendEstimator(Aer.get_backend("qasm_simulator")),primitive_seed=0)
+        """Executor with BackendEstimator initialization."""
+        return Executor(BackendEstimator(Aer.get_backend("qasm_simulator")), primitive_seed=0)
 
     @pytest.fixture(scope="module")
     def simple_circuit(self):
-        """Executor with Estimator initilization."""
+        """Creates a simple circuit for testing."""
         qc = QuantumCircuit(2)
-        qc.h([0,1])
+        qc.h([0, 1])
         return qc
 
     @pytest.fixture(scope="module")
     def observable(self):
+        """Creates a simple observable for testing."""
         return SparsePauliOp("ZZ")
 
-    @pytest.mark.parametrize("executor_str", ["ExecutorSampler", "ExecutorEstimator",
-                                            "ExecutorStatevector", "ExecutorQasm",
-                                            "ExecutorBackendSampler", "ExecutorBackendEstimator"])
+    @pytest.mark.parametrize(
+        "executor_str",
+        [
+            "ExecutorSampler",
+            "ExecutorEstimator",
+            "ExecutorStatevector",
+            "ExecutorQasm",
+            "ExecutorBackendSampler",
+            "ExecutorBackendEstimator",
+        ],
+    )
     def test_shots(self, executor_str, request):
+        """Tests of the default shots and the set_shots method work."""
 
         executor = request.getfixturevalue(executor_str)
 
-        assert_dict = {"ExecutorSampler" : None,
-        "ExecutorEstimator" : None,
-        "ExecutorStatevector" : None,
-        "ExecutorQasm" : 1024,
-        "ExecutorBackendSampler" : 1024,
-        "ExecutorBackendEstimator" : 1024}
+        assert_dict = {
+            "ExecutorSampler": None,
+            "ExecutorEstimator": None,
+            "ExecutorStatevector": None,
+            "ExecutorQasm": 1024,
+            "ExecutorBackendSampler": 1024,
+            "ExecutorBackendEstimator": 1024,
+        }
 
         assert executor.shots == assert_dict[executor_str]
         executor.set_shots(1234)
         assert executor.shots == 1234
         assert executor.get_shots() == 1234
 
-    @pytest.mark.parametrize("executor_str", ["ExecutorSampler", "ExecutorEstimator",
-                                            "ExecutorStatevector", "ExecutorQasm",
-                                            "ExecutorBackendSampler", "ExecutorBackendEstimator"])
+    @pytest.mark.parametrize(
+        "executor_str",
+        [
+            "ExecutorSampler",
+            "ExecutorEstimator",
+            "ExecutorStatevector",
+            "ExecutorQasm",
+            "ExecutorBackendSampler",
+            "ExecutorBackendEstimator",
+        ],
+    )
     def test_sampler(self, executor_str, request, simple_circuit):
+        """Tests the Executor Sampler Primitive"""
 
-        assert_dict = {"ExecutorSampler" : {0: 0.26, 1: 0.22, 2: 0.2, 3: 0.32},
-        "ExecutorEstimator" : {0: 0.26, 1: 0.22, 2: 0.2, 3: 0.32},
-        "ExecutorStatevector" : {0: 0.26, 1: 0.22, 2: 0.2, 3: 0.32},
-        "ExecutorQasm" : {3: 0.24, 2: 0.29, 1: 0.28, 0: 0.19},
-        "ExecutorBackendSampler" : {3: 0.24, 2: 0.29, 1: 0.28, 0: 0.19},
-        "ExecutorBackendEstimator" : {3: 0.24, 2: 0.29, 1: 0.28, 0: 0.19}}
+        assert_dict = {
+            "ExecutorSampler": {0: 0.26, 1: 0.22, 2: 0.2, 3: 0.32},
+            "ExecutorEstimator": {0: 0.26, 1: 0.22, 2: 0.2, 3: 0.32},
+            "ExecutorStatevector": {0: 0.26, 1: 0.22, 2: 0.2, 3: 0.32},
+            "ExecutorQasm": {3: 0.24, 2: 0.29, 1: 0.28, 0: 0.19},
+            "ExecutorBackendSampler": {3: 0.24, 2: 0.29, 1: 0.28, 0: 0.19},
+            "ExecutorBackendEstimator": {3: 0.24, 2: 0.29, 1: 0.28, 0: 0.19},
+        }
 
         executor = request.getfixturevalue(executor_str)
         executor.set_shots(100)
@@ -88,20 +111,31 @@ class TestExecutor:
         assert res.metadata[0]["shots"] == 100
         assert res.quasi_dists[0] == assert_dict[executor_str]
 
-    @pytest.mark.parametrize("executor_str", ["ExecutorSampler", "ExecutorEstimator",
-                                            "ExecutorStatevector", "ExecutorQasm",
-                                            "ExecutorBackendSampler", "ExecutorBackendEstimator"])
-    def test_executor(self, executor_str, request, simple_circuit,observable):
+    @pytest.mark.parametrize(
+        "executor_str",
+        [
+            "ExecutorSampler",
+            "ExecutorEstimator",
+            "ExecutorStatevector",
+            "ExecutorQasm",
+            "ExecutorBackendSampler",
+            "ExecutorBackendEstimator",
+        ],
+    )
+    def test_executor(self, executor_str, request, simple_circuit, observable):
+        """Tests the Executor Estimator Primitive"""
 
-        assert_dict = {"ExecutorSampler" : np.array([0.03455842]),
-        "ExecutorEstimator" : np.array([0.03455842]),
-        "ExecutorStatevector" : np.array([0.03455842]),
-        "ExecutorQasm" : np.array([0.04]),
-        "ExecutorBackendSampler" : np.array([0.04]),
-        "ExecutorBackendEstimator" : np.array([0.04])}
+        assert_dict = {
+            "ExecutorSampler": np.array([0.03455842]),
+            "ExecutorEstimator": np.array([0.03455842]),
+            "ExecutorStatevector": np.array([0.03455842]),
+            "ExecutorQasm": np.array([0.04]),
+            "ExecutorBackendSampler": np.array([0.04]),
+            "ExecutorBackendEstimator": np.array([0.04]),
+        }
 
         executor = request.getfixturevalue(executor_str)
         executor.set_shots(100)
-        res = executor.get_estimator().run(simple_circuit,observable).result()
+        res = executor.get_estimator().run(simple_circuit, observable).result()
         assert res.metadata[0]["shots"] == 100
-        assert np.allclose(assert_dict[executor_str],res.values[0])
+        assert np.allclose(assert_dict[executor_str], res.values[0])
