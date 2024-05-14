1be8c7e7184695a4bddfa35f95161c4a496c8fa3
David Kreplin <106071697+David-Kreplin@users.noreply.github.com>
2024-05-14 11:07:04 +0200

    fix for the layered_ec to be copyable (#263)
    * hashable function fix
    
    * black

--- src/squlearn/encoding_circuit/layered_encoding_circuit.py
+++ src/squlearn/encoding_circuit/layered_encoding_circuit.py
@@ -1,14 +1,15 @@
+import numpy as np
+from typing import Union, Callable
+import copy
+
+import sympy as sp
+
 from qiskit import QuantumCircuit
 from qiskit.circuit import ParameterVector
 from qiskit.circuit.library import RXGate, RYGate, RZGate, PhaseGate, UGate
 from qiskit.circuit.library import CPhaseGate, CHGate, CXGate, CYGate, CZGate
 from qiskit.circuit.library import SwapGate, CRXGate, CRYGate, CRZGate, RXXGate
 from qiskit.circuit.library import RYYGate, RZXGate, RZZGate, CUGate
-from typing import Union, Callable
-import copy
-
-# is needed for making encoding circuits with numpy functions using strings:
-import numpy as np
 
 from .encoding_circuit_base import EncodingCircuitBase
 
@@ -1268,15 +1269,31 @@ class LayeredPQC:
         """initializes a encoding circuit through a given string of gates"""
 
         def generate_function(map_string, args):
-            """Translates a string into a function"""
-            function_string = """\
-def math_function({var}):
-    return {func}
-            """.format(
-                func=map_string, var=args
-            )
-            exec(function_string, globals())
-            return math_function  # pylint: disable=undefined-variable
+            """Function for creating a callable function from a string
+
+            Args:
+                map_string (str): The string representation of the function
+                args (str): The string representation of the arguments of the function
+
+            Returns:
+                callable_function (function): The function that can be called with the arguments
+            """
+            # Split the argument string into individual variable names
+            arg_names = args.split(",")
+
+            # Remove "np." prefix
+            map_string = map_string.replace("np.", "")
+
+            # Create symbols for each variable
+            symbols = [sp.Symbol(arg) for arg in arg_names]
+
+            # Parse the map_string into a SymPy expression
+            expr = sp.sympify(map_string, evaluate=False)
+
+            # Create the function
+            callable_function = sp.lambdify(symbols, expr, modules=np)
+
+            return callable_function
 
         def get_closing_bracket_index(word, index):
             """gives to an open round bracket '(' the location of the closing bracket. This works especially, if there are more than one open brackets."""
