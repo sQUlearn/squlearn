fcdb48e5de77b8bd9a71906a72d157302b7362d0
Moritz <moritz.willmann@ipa.fraunhofer.de>
2024-05-17 09:57:02 +0200

    bump version

--- src/squlearn/__init__.py
+++ src/squlearn/__init__.py
@@ -3,7 +3,7 @@
 from .util import Executor
 from . import observables, encoding_circuit, kernel, optimizers, qnn, util
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 __all__ = [
     "Executor",
