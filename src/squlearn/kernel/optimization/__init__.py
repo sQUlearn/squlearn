0c9250714f845d18ea5c4cdd01ea2178da24c7b5
Dennis Kleinhans <dennis.kleinhans@ipa.fraunhofer.de>
2024-07-06 11:19:21 +0200

    format with black

--- src/squlearn/kernel/optimization/__init__.py
+++ src/squlearn/kernel/optimization/__init__.py
@@ -1,5 +1,4 @@
-from .kernel_optimizer import KernelOptimizer
 from .negative_log_likelihood import NLL
 from .target_alignment import TargetAlignment
 
-__all__ = ["KernelOptimizer", "NLL", "TargetAlignment"]
+__all__ = ["NLL", "TargetAlignment"]
