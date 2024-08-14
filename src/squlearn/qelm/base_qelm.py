a8b790728dcc0aa9194663b3dd6cd8ae583b390a
David Kreplin <david.kreplin@ipa.fraunhofer.de>
2024-08-14 12:44:49 +0200

    working on get/set params

--- src/squlearn/qelm/base_qelm.py
+++ src/squlearn/qelm/base_qelm.py
@@ -9,7 +9,7 @@ from qiskit.quantum_info.operators.random import random_unitary
 
 
 from ..observables.observable_base import ObservableBase
-from ..observables import CustomObservable
+from ..observables import CustomObservable, SinglePauli
 from ..encoding_circuit.encoding_circuit_base import EncodingCircuitBase
 from ..util import Executor
 from ..qnn.lowlevel_qnn import LowLevelQNN
@@ -21,9 +21,10 @@ class BaseQELM(BaseEstimator, ABC):
         encoding_circuit: EncodingCircuitBase,
         executor: Executor,
         ml_model: str = 'linear', # or 'mlp'
+        ml_model_options: Union[dict, None] = None,
         num_operators: int = 100,
         operator_seed: int = 0,
-        operators: Union[ObservableBase, list[ObservableBase]] = None,
+        operators: Union[ObservableBase, list[ObservableBase],str] = "random_paulis",
         param_ini: Union[np.ndarray, None] = None,
         param_op_ini: Union[np.ndarray, None] = None,
         parameter_seed: Union[int, None] = 0,
@@ -35,6 +36,7 @@ class BaseQELM(BaseEstimator, ABC):
         self.encoding_circuit = encoding_circuit
         self.executor = executor
         self.ml_model = ml_model
+        self.ml_model_options = ml_model_options
         self.num_operators = num_operators
         self.operator_seed = operator_seed
         self.operators = operators
@@ -43,20 +45,46 @@ class BaseQELM(BaseEstimator, ABC):
         self.parameter_seed = parameter_seed
         self.caching = caching
 
-        if self.operators is None:
-            # Generate random operators
-            paulis = random_pauli_list(self.encoding_circuit.num_qubits,self.num_operators,seed=self.operator_seed,phase=False)
-            self.operators = [CustomObservable(self.encoding_circuit.num_qubits,str(p)) for p in paulis]
+        self._initialize_observables()
+        self._initialize_lowlevel_qnn()
+        self._initialize_ml_model()
+
+    @property
+    def used_operators(self):
+        return self._operators
+
+    @property
+    def qnn(self):
+        return self._qnn
+
+    def _initialize_observables(self):
+
+        if isinstance(self.operators, str):
+            if self.operators == "random_paulis":
+                # Generate random operators
+                paulis = random_pauli_list(self.encoding_circuit.num_qubits,self.num_operators,seed=self.operator_seed,phase=False)
+                self._operators = [CustomObservable(self.encoding_circuit.num_qubits,str(p)) for p in paulis]
+            elif self.operators == "single_paulis":
+                # Generate single qubit Pauli operators
+                self._operators = []
+                for i in range(self.encoding_circuit.num_qubits):
+                    for p in ["X", "Y", "Z"]:
+                        self._operators.append(SinglePauli(self.encoding_circuit.num_qubits, i, p))
+            else:
+                raise ValueError("Invalid string for operators")
         else:
             if isinstance(self.operators, ObservableBase):
-                self.operators = [self.operators]
-            self.num_operators = len(self.operators)
+                self._operators = [self.operators]
+            elif isinstance(self.operators, list):
+                self._operators = self.operators
+            else:
+                raise ValueError("Invalid operators. Must be an ObservableBase object or a list of ObservableBase objects or None.")
+            self.num_operators = len(self._operators)
 
-        self._initialize_lowlevel_qnn()
 
     def _initialize_lowlevel_qnn(self):
         self._qnn = LowLevelQNN(
-            self.encoding_circuit, self.operators, self.executor, result_caching=self.caching
+            self.encoding_circuit, self._operators, self.executor, result_caching=self.caching
         )
 
         if self.param_ini is not None:
@@ -69,25 +97,27 @@ class BaseQELM(BaseEstimator, ABC):
                 seed=self.parameter_seed
             )
 
-        num_op_parameters = sum(operator.num_parameters for operator in self.operators)
+        num_op_parameters = sum(operator.num_parameters for operator in self._operators)
         if self.param_op_ini is not None:
             if len(self.param_ini) != self.encoding_circuit.num_parameters:
                 if num_op_parameters != len(self.param_op_ini):
                     self.param_op_ini = np.concatenate(
                         [
                             operator.generate_initial_parameters(seed=self.parameter_seed)
-                            for operator in self.operators
+                            for operator in self._operators
                         ]
                     )
         else:
             self.param_op_ini = np.concatenate(
                 [
                     operator.generate_initial_parameters(seed=self.parameter_seed)
-                    for operator in self.operators
+                    for operator in self._operators
                 ]
             )
 
-
+    @abstractmethod
+    def _initialize_ml_model(self):
+        raise NotImplementedError
 
     def get_params(self, deep: bool = True) -> dict:
         """
@@ -104,4 +134,40 @@ class BaseQELM(BaseEstimator, ABC):
 
         if deep:
             params.update(self._qnn.get_params(deep=True))
+            params.update(self._ml_model.get_params(deep=True))
+
         return params
+
+    def set_params(self: BaseQELM, **params) -> BaseQELM:
+
+        # Create dictionary of valid parameters
+        valid_params = self.get_params().keys()
+        for key in params.keys():
+            # Check if parameter is valid
+            if key not in valid_params:
+                raise ValueError(
+                    f"Invalid parameter {key!r}. "
+                    f"Valid parameters are {sorted(valid_params)!r}."
+                )
+
+        # Set parameters
+        self_params = params.keys() & self.get_params(deep=False).keys()
+        for key in self_params:
+            setattr(self, key, params[key])
+
+        # Set encoding_circuit parameters
+        ec_params = params.keys() & self._qnn.get_params(deep=True).keys()
+        if ec_params:
+            self._qnn.set_params(**{key: params[key] for key in ec_params})
+
+        initialize_qnn = False
+        if "encoding_circuit" in params or "operator" in params:
+            initialize_qnn = True
+
+        # TODO: checks
+
+        if initialize_qnn:
+            self._initialize_lowlevel_qnn()
+
+        self._initialize_observables()
+        self._initialize_ml_model()
--- src/squlearn/qelm/qelm_regressor.py
+++ src/squlearn/qelm/qelm_regressor.py
@@ -7,6 +7,7 @@ from sklearn.base import RegressorMixin
 
 from sklearn.neural_network import MLPRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.kernel_ridge import KernelRidge
 
 from ..observables.observable_base import ObservableBase
 from ..encoding_circuit.encoding_circuit_base import EncodingCircuitBase
@@ -22,14 +23,15 @@ class QELMRegressor(BaseQELM, RegressorMixin):
     Parameters:
         encoding_circuit: EncodingCircuitBase
             The encoding circuit to use for encoding the data.
-        executor: Executor  
+        executor: Executor
     """
 
     def __init__(
         self,
         encoding_circuit: EncodingCircuitBase,
         executor: Executor,
-        ml_model: str = 'linear', # or 'linear'
+        ml_model: str = 'linear',
+        ml_model_options: dict = None,
         num_operators: int = 200,
         operator_seed: int = 0,
         operators: Union[ObservableBase, list[ObservableBase]] = None,
@@ -38,11 +40,11 @@ class QELMRegressor(BaseQELM, RegressorMixin):
         parameter_seed: Union[int, None] = 0,
         caching: bool = True,
         ) -> None:
-        
         super().__init__(
             encoding_circuit,
             executor,
             ml_model,
+            ml_model_options,
             num_operators,
             operator_seed,
             operators,
@@ -51,13 +53,6 @@ class QELMRegressor(BaseQELM, RegressorMixin):
             parameter_seed,
             caching,
         )
-
-        if self.ml_model == 'mlp':
-            self._ml_model = MLPRegressor(hidden_layer_sizes=(1,1,), max_iter=300, random_state=42)
-        elif self.ml_model == 'linear':
-            self._ml_model = LinearRegression()
-        else:
-            raise ValueError("Invalid ml_model. Please choose 'mlp' or 'linear'.")
 
     def fit(self, X, y):
         """
@@ -69,7 +64,6 @@ class QELMRegressor(BaseQELM, RegressorMixin):
             y: np.ndarray
                 The target data.
         """
-
         X_qnn = self._qnn.evaluate(X, self.param_ini, self.param_op_ini, "f")["f"]
         self._ml_model.fit(X_qnn, y)
 
@@ -84,6 +78,21 @@ class QELMRegressor(BaseQELM, RegressorMixin):
         Returns:
             np.ndarray: The predicted target data.
         """
-
         X_qnn = self._qnn.evaluate(X, self.param_ini, self.param_op_ini, "f")["f"]
         return self._ml_model.predict(X_qnn)
+
+    def _initialize_ml_model(self):
+        if self.ml_model == 'mlp':
+            if self.ml_model_options is None:
+                self.ml_model_options = {}
+            self._ml_model = MLPRegressor(**self.ml_model_options)
+        elif self.ml_model == 'linear':
+            if self.ml_model_options is None:
+                self.ml_model_options = {}
+            self._ml_model = LinearRegression(**self.ml_model_options)
+        elif self.ml_model == 'kernel':
+            if self.ml_model_options is None:
+                self.ml_model_options = {}
+            self._ml_model = KernelRidge(**self.ml_model_options)
+        else:
+            raise ValueError("Invalid ml_model. Please choose 'mlp' or 'linear'.")
