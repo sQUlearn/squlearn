fix deprication issue in the approximated gradients

src/squlearn/kernel/matrix/kernel_optimizer.py
@@ -77,9 +77,8 @@ class KernelOptimizer(KernelMatrixBase):
         Set the training parameters of the encoding circuit to numerical values
 
         Args:
-            parameters (np.ndarray) :
-                Array containing numerical values to be assigned to the trainable parameters
-                of the encoding circuit
+            parameters (np.ndarray): Array containing numerical values to be assigned to
+                                     the trainable parameters of the encoding circuit
         """
         self._quantum_kernel.assign_parameters(parameters)
 