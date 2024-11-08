8c896812a4467b196a2e8d512a632b072242054f
Dennis Kleinhans <dennis.kleinhans@ipa.fraunhofer.de>
2024-11-08 12:59:25 +0100

    fix docs

--- src/squlearn/encoding_circuit/circuit_library/param_z_feature_map.py
+++ src/squlearn/encoding_circuit/circuit_library/param_z_feature_map.py
@@ -17,7 +17,7 @@ class ParamZFeatureMap(EncodingCircuitBase):
     .. plot::
 
         from squlearn.encoding_circuit import ParamZFeatureMap
-        pqc = ParamZFeatureMap(4, 2, num_layers=2, entangling=True)
+        pqc = ParamZFeatureMap(num_qubits=4, num_features=2, num_layers=2, entangling=True)
         plt = pqc.draw(output="mpl", style={'fontsize':15,'subfontsize': 10})
         plt.tight_layout()
 
