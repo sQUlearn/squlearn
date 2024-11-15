784bdbd6a66e88dd416cc8613ca4fbd80fb3365f
Dennis Kleinhans <dennis.kleinhans@ipa.fraunhofer.de>
2024-11-15 10:49:29 +0100

    turn test_encoding_slots_mismatch_error into paramerized test

--- tests/encoding_circuit/test_encoding_slots_mismatch.py
+++ tests/encoding_circuit/test_encoding_slots_mismatch.py
@@ -12,43 +12,26 @@ from squlearn.encoding_circuit import (
 from squlearn.encoding_circuit.encoding_circuit_base import EncodingSlotsMismatchError
 
 
+@pytest.mark.parametrize(
+    "circuit_class, init_params",
+    [
+        (ChebyshevPQC, {"num_qubits": 2, "num_layers": 1, "num_features": 3}),
+        (ChebyshevRx, {"num_qubits": 2, "num_layers": 1, "num_features": 3}),
+        (ChebyshevTower, {"num_qubits": 2, "num_chebyshev": 1, "num_features": 3}),
+        (HubregtsenEncodingCircuit, {"num_qubits": 2, "num_layers": 1, "num_features": 3}),
+        (KyriienkoEncodingCircuit, {"num_qubits": 2, "num_features": 3}),
+        (MultiControlEncodingCircuit, {"num_qubits": 2, "num_layers": 1, "num_features": 3}),
+        (QCNNEncodingCircuit, {"num_qubits": 2, "num_features": 3}),
+        (YZ_CX_EncodingCircuit, {"num_qubits": 2, "num_layers": 1, "num_features": 3}),
+    ],
+)
 class TestEncodingSlotsMismatchError:
-    def test_chebyshev_pqc(self):
-        circuit = ChebyshevPQC(num_qubits=2, num_layers=1, num_features=3)
-        with pytest.raises(EncodingSlotsMismatchError):
-            circuit.get_circuit([], [])
 
-    def test_chebyshev_rx(self):
-        circuit = ChebyshevRx(num_qubits=2, num_layers=1, num_features=3)
-        with pytest.raises(EncodingSlotsMismatchError):
-            circuit.get_circuit([], [])
-
-    def test_chebyshev_tower(self):
-        circuit = ChebyshevTower(num_qubits=2, num_chebyshev=1, num_features=3)
-        with pytest.raises(EncodingSlotsMismatchError):
-            circuit.get_circuit([], [])
-
-    def test_hubregtsen_encoding_circuit(self):
-        circuit = HubregtsenEncodingCircuit(num_qubits=2, num_layers=1, num_features=3)
-        with pytest.raises(EncodingSlotsMismatchError):
-            circuit.get_circuit([], [])
-
-    def test_kyriienko_encoding_circuit(self):
-        circuit = KyriienkoEncodingCircuit(num_qubits=2, num_features=3)
-        with pytest.raises(EncodingSlotsMismatchError):
-            circuit.get_circuit([], [])
-
-    def test_multi_control_encoding_circuit(self):
-        circuit = MultiControlEncodingCircuit(num_qubits=2, num_layers=1, num_features=3)
-        with pytest.raises(EncodingSlotsMismatchError):
-            circuit.get_circuit([], [])
-
-    def test_qcnn_encoding_circuit(self):
-        circuit = QCNNEncodingCircuit(num_qubits=2, num_features=3)
-        with pytest.raises(EncodingSlotsMismatchError):
-            circuit.get_circuit([], [])
-
-    def test_yz_cx_encoding_circuit(self):
-        circuit = YZ_CX_EncodingCircuit(num_qubits=2, num_layers=1, num_features=3)
+    def test_encoding_slots_mismatch_error(self, circuit_class, init_params):
+        """
+        Test that each circuit raises EncodingSlotsMismatchError
+        when the number of features does not match the number of encoding slots.
+        """
+        circuit = circuit_class(**init_params)
         with pytest.raises(EncodingSlotsMismatchError):
             circuit.get_circuit([], [])
